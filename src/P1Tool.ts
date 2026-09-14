2cfa6ad396de6aff53d7ceea4c8dc51901d1111b
ebaauw <erik.baauw@xs4all.nl>
2026-09-14 18:10:41 +0200

    Update P1Tool.ts

--- src/P1Tool.ts
+++ src/P1Tool.ts
@@ -14,7 +14,7 @@ import type { Mode } from 'hb-lib-tools/CommandLineTool'
 import { timeout } from 'hb-lib-tools'
 import { CommandLineTool, CommandLineParser, b, u } from 'hb-lib-tools/CommandLineTool'
 import { JsonFormatter } from 'hb-lib-tools/JsonFormatter'
-import { toHostString, toInt } from 'hb-lib-tools/OptionParser'
+import { toHostString, toInt, toString } from 'hb-lib-tools/OptionParser'
 
 import { P1Client } from 'hb-p1-tools/P1Client'
 
@@ -60,6 +60,7 @@ class P1Tool extends CommandLineTool {
     dsmr22: boolean
     host?: string
     serialPort?: string
+    test?: string
     timeout: integer
   }
   private p1?: P1Client
@@ -86,6 +87,9 @@ class P1Tool extends CommandLineTool {
         this.options.host = toHostString(value, { key: 'host', userInput: true  })
       })
       .flag('2', 'dsmr22', () => { this.options.dsmr22 = true })
+      .option('T', 'test', (value) => {
+        this.options.test = toString(value, { key: 'test', nonEmpty: true, userInput: true })
+      })
       .option('t', 'timeout', (value) => {
         this.options.timeout = toInt(value, { key: 'timeout', min: 1, max: 60, userInput: true })
       })
@@ -93,27 +97,35 @@ class P1Tool extends CommandLineTool {
   }
 
   async main (): Promise<void> {
-    this.parseArguments()
-    this.p1 = new P1Client({
-      dsmr22: this.options.dsmr22,
-      logger: this,
-      host: this.options.host,
-      serialPort: this.options.serialPort,
-      timeout: this.options.timeout
-    })
-    const formatter = new JsonFormatter(
-      this.options.mode === 'service'
-        ? { noWhiteSpace: true, sortKeys: true }
-        : { sortKeys: true }
-    )
-    if (this.options.mode != null) {
-      this.setOptions({ mode: this.options.mode })
-    }
-    this.p1.on('data', (data) => { this.log(formatter.stringify(data)) })
     try {
+      this.parseArguments()
+      this.p1 = new P1Client({
+        dsmr22: this.options.dsmr22,
+        logger: this,
+        host: this.options.host,
+        serialPort: this.options.serialPort,
+        timeout: this.options.timeout
+      })
+      const formatter = new JsonFormatter(
+        this.options.mode === 'service'
+          ? { noWhiteSpace: true, sortKeys: true }
+          : { sortKeys: true }
+      )
+      if (this.options.mode != null) {
+        this.setOptions({ mode: this.options.mode })
+      }
+      this.p1.on('data', (data) => { this.log(formatter.stringify(data)) })
+      if (this.options.test != null) {
+        const { telegrams } = await import('hb-p1-tools/telegrams')
+        if (!(this.options.test in telegrams)) {
+          this.error('%s: unknown test telegram', this.options.test)
+          return
+        }
+        this.p1.parseTelegram(telegrams[this.options.test])
+        return
+      }
       await this.p1.open()
     } catch (error) {
-      delete this.p1
       this.error(error)
     }
   }
